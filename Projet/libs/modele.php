15cda45ac4bb04c5d05d6c79189162d1b88db97f
LuanDELOIRE <luan.deloire@orange.fr>
2026-06-24 15:42:17 +0200

    Adaptation "rechercherMateriel" à la nvll bdd

--- Projet/libs/modele.php
+++ Projet/libs/modele.php
@@ -1,18 +1,16 @@
 <?php
     include_once("libs/maLibSQL.pdo.php");
 
-    /*
     function rechercherMateriel($recherche) {
         $recherche = addslashes($recherche);
         
-        $sql = "SELECT p.id, p.nom, p.description, p.caution, ph.url as photo_url
-                FROM produit AS p
-                LEFT JOIN photos_produit AS ph ON p.id = ph.produitId AND ph.ordre = 1 
-                WHERE p.nom LIKE '%$recherche%' OR p.description LIKE '%$recherche%'"; 
+        $sql = "SELECT p.id, p.name, p.description, p.bail, ph.url as photo_url
+                FROM product AS p
+                LEFT JOIN product_photo AS ph ON p.id = ph.product_id AND ph.index = 1 
+                WHERE p.name LIKE '%$recherche%' OR p.description LIKE '%$recherche%'"; 
                 
         return parcoursRs(SQLSelect($sql)); 
     }
-        */
 
     function listerArticlesDisponibles() {
         $sql = "SELECT p.id, p.nom, p.description, p.caution, ph.url AS photo_url
